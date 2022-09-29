cc3e60b6774f61c2a42dc2d66f43607d1cb097c9
dorianGerard <97950443+dorianGerard@users.noreply.github.com>
2022-09-29 14:36:58 +0200

    Update structure.sql

--- structure.sql
+++ structure.sql
@@ -1,138 +1,297 @@
-#------------------------------------------------------------
-#        Script MySQL.
-#------------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 22 sep. 2021 à 21:07
+-- Version du serveur : 10.4.20-MariaDB
+-- Version de PHP : 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-#------------------------------------------------------------
-# Table: LIGUE
-#------------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE LIGUE(
-        idligue  Int  Auto_increment  NOT NULL ,
-        libligue Varchar (25) NOT NULL ,
-        libsport Varchar (25) NOT NULL
-	,CONSTRAINT LIGUE_PK PRIMARY KEY (idligue)
-)ENGINE=InnoDB;
+--
+-- Base de données : `fredi21`
+--
+CREATE DATABASE IF NOT EXISTS `fredi21` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `fredi21`;
 
+-- --------------------------------------------------------
 
-#------------------------------------------------------------
-# Table: CLUB
-#------------------------------------------------------------
+--
+-- Structure de la table `adherent`
+--
 
-CREATE TABLE CLUB(
-        idclub         Int  Auto_increment  NOT NULL ,
-        libclub        Varchar (25) NOT NULL ,
-        adrpostaleclub Varchar (50) NOT NULL ,
-        idligue        Int NOT NULL
-	,CONSTRAINT CLUB_PK PRIMARY KEY (idclub)
+DROP TABLE IF EXISTS `adherent`;
+CREATE TABLE `adherent` (
+  `id_adherent` int(11) NOT NULL,
+  `nr_licence` varchar(50) DEFAULT NULL,
+  `adr1` varchar(50) DEFAULT NULL,
+  `adr2` varchar(50) DEFAULT NULL,
+  `adr3` varchar(50) DEFAULT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_club` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	,CONSTRAINT CLUB_LIGUE_FK FOREIGN KEY (idligue) REFERENCES LIGUE(idligue)
-)ENGINE=InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `club`
+--
 
-#------------------------------------------------------------
-# Table: UTILISATEUR
-#------------------------------------------------------------
+DROP TABLE IF EXISTS `club`;
+CREATE TABLE `club` (
+  `id_club` int(11) NOT NULL,
+  `lib_club` varchar(50) DEFAULT NULL,
+  `adr1` varchar(50) DEFAULT NULL,
+  `adr2` varchar(50) DEFAULT NULL,
+  `adr3` varchar(50) DEFAULT NULL,
+  `id_ligue` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE UTILISATEUR(
-        idutil       Int  Auto_increment  NOT NULL ,
-        pseudoutil   Varchar (25) NOT NULL ,
-        mdputil      Varchar (150) NOT NULL ,
-        mailutil     Varchar (150) NOT NULL ,
-        nomutil      Varchar (25) NOT NULL ,
-        prenomutil   Varchar (25) NOT NULL ,
-        isadmin      Bool NOT NULL ,
-        iscontroleur Bool NOT NULL
-	,CONSTRAINT UTILISATEUR_PK PRIMARY KEY (idutil)
-)ENGINE=InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `ligne`
+--
 
-#------------------------------------------------------------
-# Table: ADHERENT
-#------------------------------------------------------------
+DROP TABLE IF EXISTS `ligne`;
+CREATE TABLE `ligne` (
+  `id_ligne` int(11) NOT NULL,
+  `dat_ligne` date DEFAULT NULL,
+  `lib_trajet` varchar(50) DEFAULT NULL,
+  `nb_km` int(11) DEFAULT NULL,
+  `mt_km` decimal(15,2) DEFAULT NULL,
+  `mt_peage` decimal(15,2) DEFAULT NULL,
+  `mt_repas` decimal(15,2) DEFAULT NULL,
+  `mt_hebergement` decimal(15,2) DEFAULT NULL,
+  `mt_total` decimal(15,2) DEFAULT NULL,
+  `id_motif` int(11) NOT NULL,
+  `id_note` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE ADHERENT(
-        idutil       Int NOT NULL ,
-        idadherent   Int NOT NULL ,
-        numls        Varchar (50) NOT NULL ,
-        adresseadh   Varchar (150) NOT NULL ,
-        pseudoutil   Varchar (25) NOT NULL ,
-        mdputil      Varchar (150) NOT NULL ,
-        mailutil     Varchar (150) NOT NULL ,
-        nomutil      Varchar (25) NOT NULL ,
-        prenomutil   Varchar (25) NOT NULL ,
-        isadmin      Bool NOT NULL ,
-        iscontroleur Bool NOT NULL ,
-        idclub       Int NOT NULL
-	,CONSTRAINT ADHERENT_PK PRIMARY KEY (idutil)
+-- --------------------------------------------------------
 
-	,CONSTRAINT ADHERENT_UTILISATEUR_FK FOREIGN KEY (idutil) REFERENCES UTILISATEUR(idutil)
-	,CONSTRAINT ADHERENT_CLUB0_FK FOREIGN KEY (idclub) REFERENCES CLUB(idclub)
-)ENGINE=InnoDB;
+--
+-- Structure de la table `ligue`
+--
 
+DROP TABLE IF EXISTS `ligue`;
+CREATE TABLE `ligue` (
+  `id_ligue` int(11) NOT NULL,
+  `lib_ligue` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#------------------------------------------------------------
-# Table: PERIODE
-#------------------------------------------------------------
+-- --------------------------------------------------------
 
-CREATE TABLE PERIODE(
-        idperiode          Int  Auto_increment  NOT NULL ,
-        isactive           Bool NOT NULL ,
-        libperiode         Varchar (25) NOT NULL ,
-        montantforfaitaire Decimal NOT NULL ,
-        libforfait         Varchar (25) NOT NULL ,
-        prixforfait        Decimal NOT NULL
-	,CONSTRAINT PERIODE_PK PRIMARY KEY (idperiode)
-)ENGINE=InnoDB;
+--
+-- Structure de la table `motif`
+--
 
+DROP TABLE IF EXISTS `motif`;
+CREATE TABLE `motif` (
+  `id_motif` int(11) NOT NULL,
+  `lib_motif` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#------------------------------------------------------------
-# Table: NOTE
-#------------------------------------------------------------
+-- --------------------------------------------------------
 
-CREATE TABLE NOTE(
-        idnote      Int  Auto_increment  NOT NULL ,
-        indvalidite Varchar (25) NOT NULL ,
-        dateremise  Date NOT NULL ,
-        numremise   Int NOT NULL ,
-        idutil      Int NOT NULL ,
-        idperiode   Int NOT NULL
-	,CONSTRAINT NOTE_PK PRIMARY KEY (idnote)
+--
+-- Structure de la table `note`
+--
 
-	,CONSTRAINT NOTE_ADHERENT_FK FOREIGN KEY (idutil) REFERENCES ADHERENT(idutil)
-	,CONSTRAINT NOTE_PERIODE0_FK FOREIGN KEY (idperiode) REFERENCES PERIODE(idperiode)
-)ENGINE=InnoDB;
+DROP TABLE IF EXISTS `note`;
+CREATE TABLE `note` (
+  `id_note` int(11) NOT NULL,
+  `est_valide` tinyint(1) DEFAULT NULL,
+  `mt_total` decimal(15,2) DEFAULT NULL,
+  `dat_remise` date DEFAULT NULL,
+  `nr_ordre` int(11) DEFAULT NULL,
+  `id_periode` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-#------------------------------------------------------------
-# Table: MOTIF
-#------------------------------------------------------------
+--
+-- Structure de la table `periode`
+--
 
-CREATE TABLE MOTIF(
-        idmotif  Int  Auto_increment  NOT NULL ,
-        libmotif Varchar (25) NOT NULL
-	,CONSTRAINT MOTIF_PK PRIMARY KEY (idmotif)
-)ENGINE=InnoDB;
+DROP TABLE IF EXISTS `periode`;
+CREATE TABLE `periode` (
+  `id_periode` int(11) NOT NULL,
+  `lib_periode` varchar(50) DEFAULT NULL,
+  `est_active` tinyint(1) NOT NULL DEFAULT 0,
+  `mt_km` decimal(8,3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-#------------------------------------------------------------
-# Table: LIGNE
-#------------------------------------------------------------
+--
+-- Structure de la table `utilisateur`
+--
 
-CREATE TABLE LIGNE(
-        idligne       Int  Auto_increment  NOT NULL ,
-        libdep        Varchar (25) NOT NULL ,
-        datedep       Date NOT NULL ,
-        motifdep      Varchar (25) NOT NULL ,
-        fraispeage    Datetime NOT NULL ,
-        kmparcourus   Decimal NOT NULL ,
-        fraisrepas    Decimal NOT NULL ,
-        fraislogement Decimal NOT NULL ,
-        idnote        Int NOT NULL ,
-        idmotif       Int NOT NULL
-	,CONSTRAINT LIGNE_PK PRIMARY KEY (idligne)
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE `utilisateur` (
+  `id_utilisateur` int(11) NOT NULL,
+  `pseudo` varchar(50) DEFAULT NULL,
+  `mdp` varchar(255) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `role` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	,CONSTRAINT LIGNE_NOTE_FK FOREIGN KEY (idnote) REFERENCES NOTE(idnote)
-	,CONSTRAINT LIGNE_MOTIF0_FK FOREIGN KEY (idmotif) REFERENCES MOTIF(idmotif)
-)ENGINE=InnoDB;
+--
+-- Index pour les tables déchargées
+--
 
+--
+-- Index pour la table `adherent`
+--
+ALTER TABLE `adherent`
+  ADD PRIMARY KEY (`id_adherent`),
+  ADD KEY `fk_id_utilisateur2` (`id_utilisateur`),
+  ADD KEY `fk_id_club` (`id_club`);
+
+--
+-- Index pour la table `club`
+--
+ALTER TABLE `club`
+  ADD PRIMARY KEY (`id_club`),
+  ADD KEY `fk_id_ligue` (`id_ligue`);
+
+--
+-- Index pour la table `ligne`
+--
+ALTER TABLE `ligne`
+  ADD PRIMARY KEY (`id_ligne`),
+  ADD KEY `fk_id_motif` (`id_motif`),
+  ADD KEY `fk_id_note` (`id_note`);
+
+--
+-- Index pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  ADD PRIMARY KEY (`id_ligue`);
+
+--
+-- Index pour la table `motif`
+--
+ALTER TABLE `motif`
+  ADD PRIMARY KEY (`id_motif`);
+
+--
+-- Index pour la table `note`
+--
+ALTER TABLE `note`
+  ADD PRIMARY KEY (`id_note`),
+  ADD KEY `fk_id_periode` (`id_periode`),
+  ADD KEY `fk_id_utilisateur` (`id_utilisateur`);
+
+--
+-- Index pour la table `periode`
+--
+ALTER TABLE `periode`
+  ADD PRIMARY KEY (`id_periode`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id_utilisateur`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `adherent`
+--
+ALTER TABLE `adherent`
+  MODIFY `id_adherent` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `club`
+--
+ALTER TABLE `club`
+  MODIFY `id_club` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ligne`
+--
+ALTER TABLE `ligne`
+  MODIFY `id_ligne` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  MODIFY `id_ligue` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `motif`
+--
+ALTER TABLE `motif`
+  MODIFY `id_motif` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `note`
+--
+ALTER TABLE `note`
+  MODIFY `id_note` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `periode`
+--
+ALTER TABLE `periode`
+  MODIFY `id_periode` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `adherent`
+--
+ALTER TABLE `adherent`
+  ADD CONSTRAINT `fk_id_club` FOREIGN KEY (`id_club`) REFERENCES `club` (`id_club`),
+  ADD CONSTRAINT `fk_id_utilisateur2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+
+--
+-- Contraintes pour la table `club`
+--
+ALTER TABLE `club`
+  ADD CONSTRAINT `fk_id_ligue` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
+
+--
+-- Contraintes pour la table `ligne`
+--
+ALTER TABLE `ligne`
+  ADD CONSTRAINT `fk_id_motif` FOREIGN KEY (`id_motif`) REFERENCES `motif` (`id_motif`),
+  ADD CONSTRAINT `fk_id_note` FOREIGN KEY (`id_note`) REFERENCES `note` (`id_note`);
+
+--
+-- Contraintes pour la table `note`
+--
+ALTER TABLE `note`
+  ADD CONSTRAINT `fk_id_periode` FOREIGN KEY (`id_periode`) REFERENCES `periode` (`id_periode`),
+  ADD CONSTRAINT `fk_id_utilisateur` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
