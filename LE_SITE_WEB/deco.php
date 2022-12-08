ac02ffc53ec60195013f9e1e91b6ab33f5d86352
LalbaAnthony <97877743+LalbaAnthony@users.noreply.github.com>
2022-12-08 17:16:52 +0100

    Delete LE_SITE_WEB directory

--- LE_SITE_WEB/deco.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-// Detruit tous et on repart sur l'index
-session_start();
-session_unset();
-session_destroy();
-header("Location: index.php");
-?>
--- LE_SITE_WEB/test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include 'functions/dbconnect.php';
-    $laDATA = db_connect();
-    $mdp = "5cYqqgMn3unc9tA";
-    echo "<p>1. ".password_hash($mdp, PASSWORD_BCRYPT)."</p>";
-    echo "<p>2. ".password_hash($mdp, PASSWORD_BCRYPT)."</p>";
-
-?>
