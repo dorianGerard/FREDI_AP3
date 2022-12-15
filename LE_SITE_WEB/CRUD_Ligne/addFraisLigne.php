c74fc34f9b49acc4c155c94af915ffdd29f63804
dorianGerard <97950443+dorianGerard@users.noreply.github.com>
2022-12-15 14:31:16 +0100

    Update addFraisLigne.php

--- LE_SITE_WEB/CRUD_Ligne/addFraisLigne.php
+++ LE_SITE_WEB/CRUD_Ligne/addFraisLigne.php
@@ -6,19 +6,38 @@ $laDATA = db_connect();
 
 $submitAdd = isset($_POST['submitAdd']);
 
-$id_note = isset($_GET['id_note']) ? $_GET['id_note'] : NULL;
+$id_note = isset($_POST['id_note']) ? trim($_POST['id_note']) : NULL; 
+$idMotif = isset($_POST['id_motif']) ? trim($_POST['id_motif']) : NULL; 
 
-echo $id_note;
+echo $id_note."\n";
+echo $idMotif;
 
-$date = isset($_POST['Date']) ? trim($_POST['Date']) : NULL;
-$lib_trajet = isset($_POST['Nom']) ? trim($_POST['Nom']) : NULL;
-$nb_km = isset($_POST['nbKm']) ? trim($_POST['nbKm']) : NULL;
-$mt_peage = isset($_POST['mtPeage']) ? trim($_POST['mtPeage']) : NULL;
-$mt_repas = isset($_POST['mtRepas']) ? trim($_POST['mtRepas']) : NULL;
-$mt_hebergement = isset($_POST['mtHebergement']) ? trim($_POST['mtHebergement']) : NULL;
+
 
 if ($submitAdd) {
-    $sql = "INSERT INTO ligne (id_note, date_ligne, lib_trajet, nb_km, mt_peage, mt_repas, mt_hebergement) values (:id_note, :date_ligne, :lib_trajet, :nb_km, :mt_peage, :mt_repas, :mt_hebergement)";
+    $date = isset($_POST['Date']) ? trim($_POST['Date']) : NULL;
+    $lib_trajet = isset($_POST['Nom']) ? trim($_POST['Nom']) : NULL;
+    $nb_km = isset($_POST['nbKm']) ? trim($_POST['nbKm']) : NULL;
+    $mt_peage = isset($_POST['mtPeage']) ? trim($_POST['mtPeage']) : NULL;
+    $mt_repas = isset($_POST['mtRepas']) ? trim($_POST['mtRepas']) : NULL;
+    $mt_hebergement = isset($_POST['mtHebergement']) ? trim($_POST['mtHebergement']) : NULL;
+
+    $id_note = isset($_POST['id_note']) ? trim($_POST['id_note']) : NULL; 
+    $idMotif = isset($_POST['id_motif']) ? trim($_POST['id_motif']) : NULL; 
+
+    try {
+        $sql = "SELECT id_periode, mt_km FROM periode WHERE est_active = 1";
+        $result = $laDATA -> prepare($sql);
+        $result->execute();
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $ex) {
+            die("Erreur lors de la requête SQL l35: " . $ex->getMessage());
+        }
+
+    $mt_km = $row['mt_km'] * $nb_km;
+    $mt_total = $mt_repas + $mt_hebergement + $mt_peage + $mt_km;
+
+    $sql = "INSERT INTO ligne (date_ligne, lib_trajet, nb_km, mt_km, mt_peage, mt_repas, mt_hebergement, mt_total, id_motif, id_note) values (:date_ligne, :lib_trajet, :nb_km, :mt_km, :mt_peage, :mt_repas, :mt_hebergement, :mt_total, :id_motif, :id_note)";
     try {
         $result = $laDATA->prepare($sql);
         $result->execute(array(
@@ -26,9 +45,12 @@ if ($submitAdd) {
             ":date_ligne" => $date,
             ":lib_trajet" => $lib_trajet,
             ":nb_km" => $nb_km,
+            ":mt_km" => $mt_km,
             ":mt_peage" => $mt_peage,
             ":mt_repas" => $mt_repas,
-            ":mt_hebergement" => $mt_hebergement
+            ":mt_hebergement" => $mt_hebergement,
+            ":mt_total" => $mt_total,
+            ":id_motif" => $idMotif
         ));
     } catch (PDOException $e) {
         die("<p>Erreur lors de la requête SQL : " . $e->getMessage() . "</p>");
@@ -57,7 +79,10 @@ if ($submitAdd) {
         <p>Montant Repas : <br /><input type='text' name='mtRepas' required /></p>
         <p>Montant Hébergement : <br /><input type='text' name='mtHebergement' required /></p>
         <p><br /><input type='submit' name='submitAdd' value='Ajouter' /></p>
+        <?php echo "<input type='text' name='id_note' style='display:none;' value='" . $id_note . "'>";
+        echo "<input type='text' name='id_motif' style='display:none;' value='" . $idMotif . "'>"; ?>
     </form>
+    
 
 </body>
 
