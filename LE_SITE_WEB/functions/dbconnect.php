f6e687776c00138910814fcde23e765d0b8e4716
dorianGerard <97950443+dorianGerard@users.noreply.github.com>
2023-04-12 14:09:46 +0200

    Add files via upload

--- LE_SITE_WEB/functions/dbconnect.php
+++ LE_SITE_WEB/functions/dbconnect.php
@@ -3,8 +3,8 @@
 function db_connect()
 {
     $dsn = 'mysql:host=localhost;port=3306;dbname=fredi21';
-    $user = 'SELECTO';
-    $PW = '$ùS3L3C7Où$';
+    $user = 'root';
+    $PW = '';
     try {
         $laDATA = new PDO($dsn, $user, $PW, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         $laDATA->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- LE_SITE_WEB/functions/menu.php
+++ LE_SITE_WEB/functions/menu.php
@@ -1,33 +1,30 @@
-<?php
-//
-session_start();
-// include "inc/db_functions.php";
-
-// Faire une condition avec la SESSION / COOKIE pour faire apparaitre le message ligne 22 ou pas.
-?>
+<?php session_start(); ?>
 <link rel="stylesheet" href="css/styles.css">
 <header class="header">
-  <a href="" class="logo">Accueil</a>
+  <a href="index.php" class="logo">Accueil</a>
 
   <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
 
   <ul class="menu">
 
-    <li class="titre"><a href="">FREDI</a></li>
+    <li class="titre"><a href="index.php">FREDI</a></li>
     <?php
-    if ($_SESSION) {
-      echo "<li><a href='utilisateurs.php'>Liste des utilisateurs</a></li>";
-      // L’administrateur n’a pas accès aux notes.
-      if ($_SESSION['roleid'] != 3) {
-        echo "<li><a href='frais.php'>Mes notes de frais</a></li>";
-      }
-      if ($_SESSION['roleid'] == 2) {
-        echo "<li><a href='controleur.php'>Contrôleur</a></li>";
-      }
+      if (isset($_SESSION['roleid']))
+      {
+        if ($_SESSION['roleid'] == 1) {
+          echo "<li><a href='frais.php'>Mes notes de frais</a></li>";
+        }
+        if ($_SESSION['roleid'] == 2) {
+          echo "<li><a href='controleur.php'>Contrôleur</a></li>";
+          echo "<li><a href='noteFraisControleur.php'>Les notes de frais</a></li>";
+        }
+        if($_SESSION['roleid'] == 3) {
+        echo "<li><a href='utilisateurs.php'>Liste des utilisateurs</a></li>";
+        }
       echo "<li style='width: 300px;'><a href='' style='padding-top: 11px;padding-bottom: 0px;'>Bienvenue " . $_SESSION['pseudo'] . ", vous êtes connecté en tant que " . $_SESSION['rolelib'] . "</a></li>";
       echo "<li><a href='deco.php'>Déconnexion</a></li>";
-    } else {
+      } else {
       echo "<li><a href='connexion.php'>Connectez-vous</a></li>";
     } ?>
   </ul>
